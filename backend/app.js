2d7ba9a9135af8b2791c364c19b1819d465cca35
SrAdrielM <20230542@ricaldone.edu.sv>
2025-06-18 11:25:59 -0600

    gg

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,21 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-import taskRouter from "./src/routes/task.js"
+import taskRouter from "./src/routes/task.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const docApi = JSON.parse(fs.readFileSync(path.join(__dirname, "documentation.json"), "utf-8"));
 
 const app = express();
-
 app.use(express.json());
 
-app.use("/api/task", taskRouter)
+app.use("/api/task", taskRouter);
+
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(docApi));
 
 export default app;
